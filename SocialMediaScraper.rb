fda29a25535c393994d153203120b075fd07e0db
NateV <nathan.c.vogel@gmail.com>
2014-05-07 10:41:56 -0400

    movin stuff

--- SocialMediaScraper.rb
+++ SocialMediaScraper.rb
@@ -6,8 +6,8 @@ require 'json'
 require 'oauth'
 require 'debugger'
 require 'csv'
-require 'secrets.rb' #this is a file with a hash of the api keys and secrets.
-require 'organizations.rb'
+require './secrets.rb' #this is a file with a hash of the api keys and secrets.
+require './organizations.rb'
 
 class Scraper
 
