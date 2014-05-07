c839cda1e421f676d09cdb18785c0fa4a4d07cc1
NateV <nathan.c.vogel@gmail.com>
2014-05-07 13:12:26 -0400

    more orgs

--- organizations.rb
+++ organizations.rb
@@ -1,5 +1,5 @@
 def organizations 
-  JSON.parse('[{
+  JSON.parse('[{  "
       "organization": "NYCLU",
       "facebook_screen_name": "NewYorkCivilLibertiesUnion",
       "twitter_screen_name": "NYCLU"
@@ -7,6 +7,51 @@ def organizations
       "organization": "Center for Constitutional Rights",
       "facebook_screen_name": "CenterforConstitutionalRights",
       "twitter_screen_name": "theCCR"
-    }]')
+    },{
+      "organization": "The Brennan Center",
+      "facebook_screen_name": "BrennanCenter",
+      "twitter_screen_name": "BrennanCenter"
+    },{
+      "organization": "NAACP_LDF",
+      "facebook_screen_name": "naacpldf",
+      "twitter_screen_name": "NAACP_LDF"
+    },{
+      "organization": "ACLU",
+      "facebook_screen_name": "aclu.nationwide",
+      "twitter_screen_name": "ACLU"
+    },{
+      "organization": "ACLU_SoCal",
+      "facebook_screen_name": "ACLU.SoCal",
+      "twitter_screen_name": "ACLU_SoCal"
+    },{
+      "organization": "ACLU_WA",
+      "facebook_screen_name": "acluwa",
+      "twitter_screen_name": "ACLU_WA"
+    },{
+      "organization": "ACLUUtah",
+      "facebook_screen_name": "aclu.utah",
+      "twitter_screen_name": "acluutah"
+    },{
+      "organization": "National Action",
+      "facebook_screen_name": "nationalactionnetwork",
+      "twitter_screen_name": "nationalaction"
+    },{
+      "organization": "SPL Center",
+      "facebook_screen_name": "SPLCenter",
+      "twitter_screen_name": "splcenter"
+    },{
+      "organization": "Lambda Legal",
+      "facebook_screen_name": "lambdalegal",
+      "twitter_screen_name": "LambdaLegal"
+    },{
+      "organization": "Latino Justice",
+      "facebook_screen_name": "latinojustice",
+      "twitter_screen_name": "latinojustice"
+    },{
+      "organization": "CivilRights.org",
+      "facebook_screen_name": "civilandhumanrights",
+      "twitter_screen_name": "civilrightsorg"
+    }
+]')
     
 end
