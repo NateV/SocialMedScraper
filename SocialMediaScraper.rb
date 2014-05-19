69057ca4920b1642810c5daf4b14f80d58b58108
NateV <nathan.c.vogel@gmail.com>
2014-05-19 10:44:24 -0400

    added ability to specify custom groups. But no error checking, so be careful

--- SocialMediaScraper.rb
+++ SocialMediaScraper.rb
@@ -7,9 +7,70 @@ require 'oauth'
 require 'csv'
 #require './secrets.rb' if File.exists?('secrets.rb')
 require './env.rb' if File.exists?('env.rb')
-require './organizations.rb'
+
 
 class Scraper
+
+  def initialize(*args)
+    if args.length==0 then
+      @organizations =   JSON.parse('[{
+      "organization": "NYCLU",
+      "facebook_screen_name": "NewYorkCivilLibertiesUnion",
+      "twitter_screen_name": "NYCLU"
+    },{
+      "organization": "Center for Constitutional Rights",
+      "facebook_screen_name": "CenterforConstitutionalRights",
+      "twitter_screen_name": "theCCR"
+    },{
+      "organization": "The Brennan Center",
+      "facebook_screen_name": "BrennanCenter",
+      "twitter_screen_name": "BrennanCenter"
+    },{
+      "organization": "NAACP_LDF",
+      "facebook_screen_name": "naacpldf",
+      "twitter_screen_name": "NAACP_LDF"
+    },{
+      "organization": "ACLU",
+      "facebook_screen_name": "aclu.nationwide",
+      "twitter_screen_name": "ACLU"
+    },{
+      "organization": "ACLU_SoCal",
+      "facebook_screen_name": "ACLU.SoCal",
+      "twitter_screen_name": "ACLU_SoCal"
+    },{
+      "organization": "ACLU_WA",
+      "facebook_screen_name": "acluwa",
+      "twitter_screen_name": "ACLU_WA"
+    },{
+      "organization": "ACLUUtah",
+      "facebook_screen_name": "aclu.utah",
+      "twitter_screen_name": "acluutah"
+    },{
+      "organization": "National Action",
+      "facebook_screen_name": "nationalactionnetwork",
+      "twitter_screen_name": "nationalaction"
+    },{
+      "organization": "SPL Center",
+      "facebook_screen_name": "SPLCenter",
+      "twitter_screen_name": "splcenter"
+    },{
+      "organization": "Lambda Legal",
+      "facebook_screen_name": "lambdalegal",
+      "twitter_screen_name": "LambdaLegal"
+    },{
+      "organization": "Latino Justice",
+      "facebook_screen_name": "latinojustice",
+      "twitter_screen_name": "latinojustice"
+    },{
+      "organization": "CivilRights.org",
+      "facebook_screen_name": "civilandhumanrights",
+      "twitter_screen_name": "civilrightsorg"
+    }
+]')
+    else
+      @organizations = JSON.parse(args[0])
+    end
+  end# of initialize
 
   #for twitter:
   # Exchange your oauth_token and oauth_token_secret for an AccessToken instance.
@@ -90,7 +151,7 @@ class Scraper
   def scrape
     date = Date.today
     #puts organizations
-    results = organizations.map do |organization|
+    results = @organizations.map do |organization|
       output = {date: "#{date.month}-#{date.day}-#{date.year}",
       			organization: organization["organization"]}
       output.merge! facebook_scrape(organization["facebook_screen_name"])
--- app.rb
+++ app.rb
@@ -5,8 +5,20 @@ get '/' do
   erb :index
 end
 
-get '/stats.csv' do 
+get '/standardStats.csv' do 
   scraper = Scraper.new
+  @results = scraper.scrape
+  @results = Scraper.as_csv(@results)
+  erb :stats_csv
+end
+
+get '/customOrgs' do
+  erb :customOrgs
+end
+
+post '/customOrgs' do 
+  organization_string = params[:organizations]
+  scraper = Scraper.new(organization_string)
   @results = scraper.scrape
   @results = Scraper.as_csv(@results)
   erb :stats_csv
